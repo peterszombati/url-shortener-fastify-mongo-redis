30ace5cd01d3999b11fefeb8ee03fe98143e3b3e
Peter Szombati <szombatipeti7@gmail.com>
2025-04-15 18:59:37 +0200

    make less db query in case if there is no redirect

--- nodejs/src/apps/analytics/handleAnalytics.ts
+++ nodejs/src/apps/analytics/handleAnalytics.ts
@@ -1,12 +1,13 @@
 import { analyticsRedirects } from './db/analytics-redirects';
 
 export async function handleAnalytics(alias: string) {
-  const [totalRedirects, lastRedirects] = await Promise.all([
-    analyticsRedirects.model.countDocuments({ alias }),
-    analyticsRedirects.model
+  const totalRedirects = await analyticsRedirects.model.countDocuments({ alias })
+  const lastRedirects = totalRedirects > 0 ? await analyticsRedirects.model
       .find({ alias }, {}, { projection: { createdAt: 1, _id: 0 } })
       .sort({ createdAt: -1 })
-      .limit(5),
-  ]);
-  return { totalRedirects, lastRedirects: lastRedirects.map((i) => i.createdAt) };
+      .limit(5) : []
+  return {
+    totalRedirects,
+    lastRedirects: lastRedirects.map((i) => i.createdAt)
+  };
 }
