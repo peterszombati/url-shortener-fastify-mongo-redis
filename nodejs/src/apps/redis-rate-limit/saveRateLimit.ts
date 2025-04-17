8902dfaef5f90a3e07a32c7bb234063319f54c14
Peter Szombati <szombatipeti7@gmail.com>
2025-04-17 14:21:07 +0200

    .

--- nodejs/src/apps/redis-rate-limit/saveRateLimit.ts
+++ nodejs/src/apps/redis-rate-limit/saveRateLimit.ts
@@ -2,12 +2,11 @@ import { luaScript } from '../redis/luaScript';
 
 // language=Lua
 const script = `  local current = redis.call("INCR", KEYS[1] .. ":created")
-  local lock = redis.call("DECR", KEYS[1] .. ":lock")
+  if redis.call("EXISTS", KEYS[1] .. ":lock") == 1 then
+    redis.call("DECR", KEYS[1] .. ":lock")
+  end
   if current == 1 then
     redis.call("EXPIRE", KEYS[1] .. ":created", 86400)
-  end
-  if lock == 1 then
-    redis.call("EXPIRE", KEYS[1] .. ":lock", 30)
   end
   return 1`;
 
