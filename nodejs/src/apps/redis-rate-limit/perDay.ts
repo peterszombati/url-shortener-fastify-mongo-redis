bfccfd8b00ef6de58b58cf38cb684237403f61f9
Peter Szombati <szombatipeti7@gmail.com>
2025-04-17 18:42:31 +0200

    .

--- nodejs/src/apps/redis-rate-limit/perDay.ts
+++ nodejs/src/apps/redis-rate-limit/perDay.ts
@@ -1,15 +1,17 @@
 import { luaScript } from '../redis/luaScript';
 
 // language=Lua
-const script = `  local current = redis.call("INCR", KEYS[1] .. ":lock")
-  local created = redis.call("GET", KEYS[1] .. ":created")
-  if current == 1 then
-    redis.call("EXPIRE", KEYS[1] .. ":lock", 30)
+const script = `  local limit = tonumber(ARGV[1])
+  local created = tonumber(redis.call("GET", KEYS[1] .. ":created"))
+  if created and created >= limit then
+    return 0
   end
+  local current = redis.call("INCR", KEYS[1] .. ":lock")
+  redis.call("EXPIRE", KEYS[1] .. ":lock", 30)
   if created then
-    current = current + tonumber(created)
+    current = current + created
   end
-  if current > tonumber(ARGV[1]) then
+  if current > limit then
     return 0
   end
   return 1`;
